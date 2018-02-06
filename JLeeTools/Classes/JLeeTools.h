62b69a18eb5f19b406e6ca2727f06b38cca984e5
Jack <lijie9228@hotmail.com>
2018-02-06 17:15:17 +0800

    导入问题

--- JLeeTools/Classes/JLeeTools.h
+++ JLeeTools/Classes/JLeeTools.h
@@ -6,11 +6,11 @@
 //
 
 #import "JLeeDefine.h"
-#import "JLeeCategory /NSString+JLeeTools.h"
-#import "JLeeCategory /NSObject+Tools.h"
-#import "JLeeCategory /UILabel+Tools.h"
-#import "JLeeCategory /UIViewController+JLeeTools.h"
-#import "JLeeCategory /UIViewController+ChangeScrollColor.h"
+#import "NSString+JLeeTools.h"
+#import "NSObject+Tools.h"
+#import "UILabel+Tools.h"
+#import "UIViewController+JLeeTools.h"
+#import "UIViewController+ChangeScrollColor.h"
 
 //#import "NSString+JLeeTools.h"
 //#import ""
